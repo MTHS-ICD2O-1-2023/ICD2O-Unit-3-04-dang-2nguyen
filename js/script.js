1d08399cf9502d769a31138d6cde1154d8b9edbb
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-04-10 18:25:19 +0000

    fdbvgdsfbdf

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateTheCel() {
+
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value)
+
+
+  // process
+  const celcius = (fahrenheit - 32 ) * 5.0/9.0
+
+
+  // output
+  document.getElementById('celcius').innerHTML = 'celcius is : ' + celcius.toFixed(3)
+}
